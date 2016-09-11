038b9b722c7081d10ce59ee8c77aaaefcb5df556
einnuJ <junnie.sim@stonybrook.edu>
2016-09-10 21:24:39 -0400

    integrated flask templates; redirects to index.html, but cannot access any other resource left in web (css, js, fonts, res). Moving to static as per Flask documentation recommendation

--- src/python/ContactBox.py
+++ src/python/ContactBox.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import render_template
 from flask import request
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    error = None
+    return render_template('index.html')
 
 
 @app.route('/contactme', methods=['POST'])
