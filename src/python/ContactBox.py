f42ab1c40f3e0a871a63d0fe056c3153003e8e2a
EVE-PC\Junnie <junniesim91@gmail.com>
2016-09-16 03:41:23 -0400

    Testing the SMTP, but running into (network?) issues on Windows. In the process of porting over to remote server for testing under UNIX.

--- src/python/ContactBox.py
+++ src/python/ContactBox.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+from smtplib import SMTP
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
@@ -14,14 +16,27 @@ def process_message():
     error = None
 
     if request.method == 'POST':
-        return message_posted()
+        return prepare_email()
 
 
-def message_posted():
-    sender = request.form['name']
-    email = request.form['email']
-    business = request.form['business']
-    msg = request.form['message']
+def prepare_email():
+    email = {
+        'email': request.form['email'],
+        'subject': "Message from %s" % request.form['name'],
+        'message': request.form['message']
+    }
+    if request.form['business']:
+        email['subject'] = "(BUS) %s" % email['subject']
 
-    return jsonify(response="success?")
+    if send_email(email):
+        return jsonify(response="success?")
 
+
+def send_email(email_dict):
+    msg = MIMEText(email_dict['message'])
+    msg['To'] = "CHANGE THIS TO A REAL ADDRESS"
+    msg['From'] = "jsim.me : %s" % email_dict['email']
+    msg['Subject'] = email_dict['subject']
+
+    with SMTP('localhost') as smtp:
+        return smtp.send_message(msg)
