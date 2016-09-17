e0097c48bb007acb6f9c2ef6eb2c220e72920306
EVE-PC\Junnie <junniesim91@gmail.com>
2016-09-16 21:55:13 -0400

    The code had to be modified because I didn't want to set up an entire SMTP server on DigitalOcean; instead opting for the native UNIX sendmail application. Left the SMTP option as an uncalled separate method. Confirmed sending emails work. Next: validation, reCAPTCHA

--- src/python/ContactBox.py
+++ src/python/ContactBox.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from smtplib import SMTP
 from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 
@@ -22,21 +23,29 @@ def process_message():
 def prepare_email():
     email = {
         'email': request.form['email'],
-        'subject': "Message from %s" % request.form['name'],
+        'subject': "jsim: Message from %s" % request.form['name'],
         'message': request.form['message']
     }
-    if request.form['business']:
+    if request.form['business'] == 'true':
         email['subject'] = "(BUS) %s" % email['subject']
 
     if send_email(email):
-        return jsonify(response="success?")
+        return jsonify(response="200")
 
 
 def send_email(email_dict):
     msg = MIMEText(email_dict['message'])
-    msg['To'] = "CHANGE THIS TO A REAL ADDRESS"
-    msg['From'] = "jsim.me : %s" % email_dict['email']
+    msg['To'] = "junniesim91@gmail.com"
+    msg['From'] = email_dict['email']
     msg['Subject'] = email_dict['subject']
+    return send_via_sendmail(msg)
 
+
+def send_via_sendmail(msg):
+    p = Popen(["usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+    return p.communicate(msg.as_string())
+
+
+def send_via_smtp(msg):
     with SMTP('localhost') as smtp:
         return smtp.send_message(msg)
